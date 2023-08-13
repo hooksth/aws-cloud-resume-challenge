2f0055e5f97979a79a41005dce9c8f0f679c9a5d
Tavaris Hooks <hookstavaris@gmail.com>
2023-08-13 13:55:19 -0400

    troubleshooting

---
+++ HTML-Resume/index.js
@@ -0,0 +1,8 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://krwo73mzyjw5dn6xrlhht3qfxm0isekf.lambda-url.us-east-1.on.aws/")
+    let data = await response.json()
+    counter.innerHTML = ` Views: ${data}`;
+}
+
+updateCounter();
