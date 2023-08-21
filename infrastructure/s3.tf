24bfabf9549630c7f4d497570da4f06660762c6d
Tavaris Hooks <hookstavaris@gmail.com>
2023-08-21 18:14:15 -0400

    test

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -47,3 +47,29 @@ resource "aws_s3_bucket" "infrastructure" {
   }
 }
 
+resource "aws_s3_bucket" "test" {
+  bucket = "doing-testing89348"
+
+  lifecycle {
+    prevent_destroy = false
+
+  }
+
+  versioning {
+    enabled  = true
+
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  tags = {
+    "name" = "infrastructure"
+    "infra" = "back-endddd"
+  }
+}
+
